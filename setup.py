9d0420328f4d66bdadb35d03cba0cadb25e539dc
Chris Streeter <chris@chrisstreeter.com>
2012-08-03 12:16:15 -0700

    Version bump.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-haystack-panel',
-    version='0.1.0',
+    version='0.1.1',
     description='A Django Debug Toolbar panel for Haystack',
     long_description=open('README.md').read(),
     author='Chris Streeter',
